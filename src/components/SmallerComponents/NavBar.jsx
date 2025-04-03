ef7b3293bbc99a498a2e5dbd5657879939211182
atikP <atikpatel@hotmail.co.uk>
2025-04-03 19:55:27 +0100

    navbar fix

--- src/components/SmallerComponents/NavBar.jsx
+++ src/components/SmallerComponents/NavBar.jsx
@@ -126,7 +126,7 @@ function NavBar({ setOpen }) {
         <div className="fixed inset-0 z-50" />
         <DialogPanel className="fixed inset-y-0 right-0 z-50 w-full overflow-y-auto bg-white px-6 py-6 sm:max-w-sm sm:ring-1 sm:ring-gray-900/10">
           <div className="flex items-center justify-between">
-            <Link to="/" className="-m-1.5 p-1.5">
+            <Link to="/" className="-m-1.5 p-1.5"onClick={() => setMobileMenuOpen(false)}>
               <img alt="" src={logo} className="h-8 w-auto" />
             </Link>
             <button
